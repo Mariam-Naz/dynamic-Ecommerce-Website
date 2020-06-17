c392a4d69a1c1d51e1b49f99f193dbee216977e7
unknown <mariamnaz212@gmail.com>
2020-06-17 22:09:00 +0500

    connecting  database and connecting

--- resources/config.php
+++ resources/config.php
@@ -1,11 +1,28 @@
 <?php
 
-defined("DS") ? null : define("DS",DIRECTORY_SEPARATOR);
+ob_start();
+session_start();
+
+defined("DS") ? null : define("DS",DIRECTORY_SEPARATOR); 
 
 defined("TEMPLATE_FRONT") ? null : define("TEMPLATE_FRONT", __DIR__ . DS . "templates/front");
 
-defined("TEMPLATE_BACK") ? null : define("DSTEMPLATE_BACK",__DIR__ . DS . "templates/back");
+defined("TEMPLATE_BACK") ? null : define("TEMPLATE_BACK",__DIR__ . DS . "templates/back");
 
-echo TEMPLATE_FRONT;
+defined("DB_HOST") ? null : define("DB_HOST","localhost"); 
+
+defined("DB_USER") ? null : define("DB_USER", "root");
+
+defined("DB_PASS") ? null : define("DB_PASS","");
+
+defined("DB_NAME") ? null : define("DB_NAME","ecom_db");
+
+$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
+
+require_once("functions.php");
+
+
+
+// echo DIRECTORY_SEPARATOR; /
 
 ?>
---
+++ resources/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+if($connection){
+    echo "connection established";
+}
+
+echo "from function";
+?>
