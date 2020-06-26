0ff3e9962096645045a419ca7c8b9fa617c24dc3
unknown <mariamnaz212@gmail.com>
2020-06-26 17:28:22 +0500

    update praise.php

--- resources/templates/front/praise.php
+++ resources/templates/front/praise.php
@@ -11,22 +11,28 @@
         margin-bottom: 6%;
         color:black;
     }
+    i{
+        font-size: 2rem;
+    }
 </style>
 <div class='wrap'>
 <div class="row">
 
     <div class='card col-lg-4 col-md-6'>
-            <h3>BEST OFFER</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr sadipscing elitr</p>
+            <i class="fas fa-heart"></i>
+            <h3>We Love Our Customer</h3>
+            <p>We know to provide the best possible service ever</p>
     </div>
     <div class='card col-lg-4 col-md-6' style="background-color:#dc3545; color: white;">
-            <h3>BEST PRICES</h3>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr sadipscing elitr</p>
+            <i class="fas fa-tag"></i>
+            <h3>Best Prices</h3>
+            <p>Compare us with another site, who have the best prices</p>
     </div>
     <div class='card col-lg-4 col-md-12'>
-            <h3>100% ORIGINAL</h3>
+            <i class="fas fa-thumbs-up"></i>
+            <h3>100% Origial</h3>
             <div class = 'deco'></div>
-            <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr sadipscing elitr</p>
+            <p>We just offer you the best and original products</p>
     </div>
 </div>
 </div>
