868fbbbebc750e2a559a626282974a76d464f89a
Maaz Shah <msmunami63@gmail.com>
2020-06-29 20:45:34 +0500

    Cart.php --> Showing the items in cart dynamically

--- public/cart.php
+++ public/cart.php
@@ -29,23 +29,33 @@ if(isset($_GET['delete'])){
 }
 
 function cart(){
-    $query = query("SELECT * FROM products");
-    confirm($query);
 
-    while($row = mysqli_fetch_array($query)){
-        $product = <<<DELIMETER
-        <tr>
-                <td>{$row['product_title']}</td>
-                <td>$23</td>
-                <td>3</td>
-                <td>2</td>
-                <td><a class='btn btn-warning' href="cart.php?remove={$row['product_id']}"><span class = 'glyphicon glyphicon-minus'></span></a>
-                <a class='btn btn-success' href="cart.php?add={$row['product_id']}"><span class = 'glyphicon glyphicon-plus'></span></a>
-                <a class='btn btn-danger' href="cart.php?delete={$row['product_id']}"><span class = 'glyphicon glyphicon-remove'></span></a></td>
-              
-            </tr>
-DELIMETER;
-echo $product;
+    foreach ($_SESSION as $name => $value) {
+        if($value > 0){
+            if(substr($name,0, 8) == "product_"){
+                $length = strlen($name)- 8;
+                $id = substr($name, 8, $length);
+                $query = query("SELECT * FROM products WHERE product_id = " . escape($id) . " ");
+                confirm($query);
+    
+        while($row = mysqli_fetch_array($query)){
+            $product = <<<DELIMETER
+            <tr>
+                    <td>{$row['product_title']}</td>
+                    <td>$23</td>
+                    <td>3</td>
+                    <td>2</td>
+                    <td><a class='btn btn-warning' href="cart.php?remove={$row['product_id']}"><span class = 'glyphicon glyphicon-minus'></span></a>
+                    <a class='btn btn-success' href="cart.php?add={$row['product_id']}"><span class = 'glyphicon glyphicon-plus'></span></a>
+                    <a class='btn btn-danger' href="cart.php?delete={$row['product_id']}"><span class = 'glyphicon glyphicon-remove'></span></a></td>
+                  
+                </tr>
+        DELIMETER;
+        echo $product;
+        }
+    
+            }   
+        }
     }
 }
 ?>
