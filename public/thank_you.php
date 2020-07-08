5d0e5a36a8c24049c1de98974efae1c17d5ae561
unknown <mariamnaz212@gmail.com>
2020-07-09 00:01:58 +0500

    Customize the thank_you.php page

--- public/thank_you.php
+++ public/thank_you.php
@@ -1,6 +1,13 @@
 <?php require_once('../resources/config.php'); ?>
 <?php include(TEMPLATE_FRONT . DS . "header.php") ?>
 
-<h1>Thank you</h1>
 <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>
-<lottie-player src="https://assets9.lottiefiles.com/temp/lf20_sTumYD.json"  background="transparent"  speed="1"  style="width: 50%; height: 50%;"    autoplay></lottie-player>
+<lottie-player src="https://assets10.lottiefiles.com/packages/lf20_NcXIOl.json"  background="#3490dc"  speed="1"  style="width: 100%; height: 90%; position: absolute;"  loop autoplay></lottie-player>
+<div class='container'>
+<div class="row">
+      <div class="col-lg-12 text-center">
+                    <h1 class="section-heading" style="margin-top: 20rem; color:white; font-size:10rem;">Thank You</h1>
+                </div>
+    </div>
+</div>
+
