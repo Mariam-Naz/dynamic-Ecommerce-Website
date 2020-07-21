8c1e03c297a565b430d2edc2052726df3ccef7e6
unknown <mariamnaz212@gmail.com>
2020-07-21 19:54:34 +0500

    update contact page

--- public/contact.php
+++ public/contact.php
@@ -9,19 +9,19 @@
 
          <!-- Contact Section -->
 
-        <div class="container">
+        <div class="container contact">
             <div class="row">
                 <div class="col-lg-12 text-center">
-                    <h2 class="section-heading">Contact Us</h2>
-                    <!-- <h3 class="section-subheading"><?php display_message(); ?></h3> -->
+                    <h1 class="section-heading">Contact Us</h1>
+                    <h3 class="section-subheading"><?php display_message(); ?></h3>
                 </div>
             </div>
             <div class="row">
                 <div class="col-lg-12">
-                    <form name="sentMessage" id="contactForm" method="POST" >
+                    <form name="sentMessage" id="contactForm" method="POST" autocomplete="off">
                         <?php send_message(); ?>
                         <div class="row">
-                            <div class="col-md-6">
+                            <div class="col-sm-4 col-sm-offset-4">
                                 <div class="form-group">
                                     <input type="text" name="name" class="form-control" placeholder="Your Name *" id="name" required data-validation-required-message="Please enter your name.">
                                     <p class="help-block text-danger"></p>
@@ -34,10 +34,8 @@
                                     <input type="text" name="subject" class="form-control" placeholder="Your Subject" id="phone" required data-validation-required-message="Please enter your phone number.">
                                     <p class="help-block text-danger"></p>
                                 </div>
-                            </div>
-                            <div class="col-md-6">
                                 <div class="form-group">
-                                    <textarea class="form-control" name="message" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
+                                    <textarea rows='10' class="form-control" name="message" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
                                     <p class="help-block text-danger"></p>
                                 </div>
                             </div>
