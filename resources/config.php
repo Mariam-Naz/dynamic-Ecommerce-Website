f95a88a72334df3ec45300b20becd21e25b1435f
unknown <mariamnaz212@gmail.com>
2020-08-12 18:26:40 +0500

    add {}

--- resources/config.php
+++ resources/config.php
@@ -18,7 +18,7 @@ defined("DB_HOST") ? null : define("DB_HOST","localhost");
 
 defined("DB_USER") ? null : define("DB_USER", "root");
 
-defined("DB_PASS") ? null : define("DB_PASS","alif");
+defined("DB_PASS") ? null : define("DB_PASS","");
 
 defined("DB_NAME") ? null : define("DB_NAME","ecom_db");
 
