77dfc8e34a91eb3476ea7d08a87fb95485b167e1
MaazShah2060 <msmunami63@gmail.com>
2020-09-05 13:29:02 +0500

    Minimizing code

--- index.php
+++ index.php
@@ -140,26 +140,7 @@ padding-right: 0px">
 <div class='row'>
     <div class="col-lg-12">
         
-    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-    <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="4"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox" style="height: 450px;">
-    <?php
-        showSliderInFront();
-      ?>
-    </div>
-    <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-    </a>
-    <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-    </a>
-</div>
+    <?php include(TEMPLATE_FRONT . DS . "slider.php") ?>
 
     </div>
 </div>
